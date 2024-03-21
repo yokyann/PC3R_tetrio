33d651f87f0bd5953cf34b05d8340e95c7a047f7
yokyann <67327884+yokyann@users.noreply.github.com>
2024-03-21 10:56:44 +0100

    Create main.go

---
+++ server/cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/yokyann/PC3R_tetrio/server/internal/adapter/handler/"
+)
+
+func main() {
+	http.HandleFunc("/fetch-username", http.FetchUsernameHandler)
+
+	log.Println("Server is running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
