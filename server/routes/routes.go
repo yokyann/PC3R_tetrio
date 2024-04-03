ec4c12d82ba98dc181f0f09a44395eb28f64c3bb
yokyann <67327884+yokyann@users.noreply.github.com>
2024-04-03 16:13:39 +0200

    Add files via upload

---
+++ server/routes/routes.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	. "github.com/yokyann/PC3R_tetrio/server/database"
+	. "github.com/yokyann/PC3R_tetrio/server/models"
+)
+
+func ListUsers(c *gin.Context) {
+	listusers := List_Users()
+	if listusers == nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "List is empty"})
+		return
+	}
+	//Response to api
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Data fetched successfully", "data": listusers})
+}
+
+func CreateUser(c *gin.Context) {
+	var user User
+	err := c.BindJSON(&book)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Bad request"})
+		return
+	}
+	id := Create_User(user)
+	//end mongo queries
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "User Created Successful", "data": id})
+}
+
+func FindUser(c *gin.Context) {
+	pseudo := c.Param("pseudo")
+	user := Find_User(pseudo)
+	if user == nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
+		return
+	}
+	//end mongo queries
+	c.IndentedJSON(http.StatusOK, gin.H{"data": user})
+}
