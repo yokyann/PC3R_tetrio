6617d5b004e5b51205f47237d4c4894cecc00bfc
yokyann <67327884+yokyann@users.noreply.github.com>
2024-04-03 15:01:12 +0200

    Add files via upload

---
+++ server/routes/routes.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	. "github.com/yokyann/PC3R_tetrio/server/database"
+	. "github.com/yokyann/PC3R_tetrio/server/models"
+)
+
+func ListBooks(c *gin.Context) {
+	listbooks := List_Books()
+	if listbooks == nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "List is empty"})
+		return
+	}
+	//Response to api
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Data fetched successfully", "data": listbooks})
+}
+
+func CreateBook(c *gin.Context) {
+	var book Book
+	err := c.BindJSON(&book)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Bad request"})
+		return
+	}
+	id := Create_Book(book)
+	//end mongo queries
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "Book Created Successful", "data": id})
+}
+
+func FindBook(c *gin.Context) {
+	name := c.Param("name")
+	book := Find_Book(name)
+	if book == nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	//end mongo queries
+	c.IndentedJSON(http.StatusOK, gin.H{"data": book})
+}
