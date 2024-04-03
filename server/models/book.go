ce82e6c1b9d007eeca0ab8a03879f3d93197a0c1
yokyann <67327884+yokyann@users.noreply.github.com>
2024-04-03 16:13:20 +0200

    Delete server directory

--- server/models/book.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Book struct {
-	Id   	primitive.ObjectID 		`json:"id"`
-	Name 	string 					`json:"name"`
-	Price 	int						`json:"price"`
-}
