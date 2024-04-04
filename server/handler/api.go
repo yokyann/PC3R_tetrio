cc3eec8b9eff3a0a3a2c8caacdfac0923eebf11f
yokyann <67327884+yokyann@users.noreply.github.com>
2024-04-04 14:08:50 +0200

    Add files via upload

---
+++ server/handler/api.go
@@ -0,0 +1,39 @@
+// routes/api.go
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	. "github.com/yokyann/PC3R_tetrio/server/models"
+)
+
+var uri = "https://ch.tetr.io/api/users"
+
+func FetchPlayerUsername(username string) (string, error) {
+	apiUrl := uri + "/" + username
+
+	resp, err := http.Get(apiUrl)
+	if err != nil {
+		return "", fmt.Errorf("failed to make GET request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return "", fmt.Errorf("API request failed with status %d: %s", resp.StatusCode, string(body))
+	}
+
+	var playerInfoResp PlayerInfoResponse
+	if err := json.NewDecoder(resp.Body).Decode(&playerInfoResp); err != nil {
+		return "", fmt.Errorf("failed to parse JSON response: %w", err)
+	}
+
+	if !playerInfoResp.Success {
+		return "", fmt.Errorf("API request unsuccessful: %s", playerInfoResp.Error)
+	}
+
+	return playerInfoResp.Data.Player.Username, nil
+}
---
+++ server/routes/api.go
@@ -0,0 +1,25 @@
+// routes/api.go
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/yokyann/PC3R_tetrio/server/handler")
+
+func FetchUsernameHandler(w http.ResponseWriter, r *http.Request) {
+	// Extraire le paramètre "username" de la requête HTTP
+	username := r.URL.Query().Get("username")
+
+	// Appeler la fonction FetchPlayerUsername avec le paramètre "username"
+	username, err := routes.FetchPlayerUsername(username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Écrire la réponse JSON avec le username récupéré
+	response := map[string]string{"username": username}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
